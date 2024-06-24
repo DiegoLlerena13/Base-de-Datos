68a004657d7155cf6dd14d99b4ac46ea5a8fb84c
Cistian Saya <saya.cristian123@gmail.com>
2024-06-23 23:59:08 -0500

    Template municipio funcional

--- Municipio/Municipio.py
+++ Municipio/Municipio.py
@@ -17,7 +17,7 @@ app.secret_key = 'your_secret_key'  # Clave secreta para usar flash messages
 @app.route('/')
 def home():
     cursor = db.database.cursor()
-    cursor.execute("SELECT * FROM region")
+    cursor.execute("SELECT * FROM municipio")
     myresult = cursor.fetchall()
 
     insertObject = []
@@ -27,73 +27,73 @@ def home():
     cursor.close()
     return render_template('Municipio.html', data=insertObject)
 
-@app.route('/region', methods=['POST'])
-def addregion():
-    codreg = request.form['codreg']
-    nomreg = request.form['nomreg']
+@app.route('/municipio', methods=['POST'])
+def add_municipio():
+    codmun = request.form['codmun']
+    nommun = request.form['nommun']
     
-    if codreg and nomreg:
+    if codmun and nommun:
         cursor = db.database.cursor()
 
         # Verificar si el código ya existe
-        cursor.execute("SELECT * FROM region WHERE RegCod = %s", (codreg,))
+        cursor.execute("SELECT * FROM municipio WHERE MunCod = %s", (codmun,))
         existing_cod = cursor.fetchone()
         
         # Verificar si el nombre ya existe
-        cursor.execute("SELECT * FROM region WHERE RegNom = %s", (nomreg,))
+        cursor.execute("SELECT * FROM municipio WHERE MunNom = %s", (nommun,))
         existing_nom = cursor.fetchone()
         
         if existing_cod:
-            flash('El código de región ya existe. Por favor ingrese un código diferente.')
+            flash('El código de municipio ya existe. Por favor ingrese un código diferente.')
             cursor.close()
             return redirect(url_for('home'))
         elif existing_nom:
-            flash('El nombre de la región ya existe. Por favor ingrese un nombre diferente.')
+            flash('El nombre del municipio ya existe. Por favor ingrese un nombre diferente.')
             cursor.close()
             return redirect(url_for('home'))
 
-        sql = "INSERT INTO region (RegCod, RegNom, RegEstReg) VALUES (%s, %s, 'A')"
-        data = (codreg, nomreg)
+        sql = "INSERT INTO municipio (MunCod, MunNom, MunEstReg) VALUES (%s, %s, 'A')"
+        data = (codmun, nommun)
         cursor.execute(sql, data)
         db.database.commit()
         cursor.close()
-        flash('Región insertada exitosamente.')
+        flash('Municipio insertado exitosamente.')
     return redirect(url_for('home'))
 
-@app.route('/delete/<string:codreg>')
-def delete(codreg):
+@app.route('/delete/<string:codmun>')
+def delete_municipio(codmun):
     cursor = db.database.cursor()
-    sql = "DELETE FROM region WHERE RegCod = %s"
-    data = (codreg,)
+    sql = "DELETE FROM municipio WHERE MunCod = %s"
+    data = (codmun,)
     cursor.execute(sql, data)
     db.database.commit()
     return redirect(url_for('home'))
 
-@app.route('/edit/<string:codreg>', methods=['POST'])
-def edit(codreg):
+@app.route('/edit/<string:codmun>', methods=['POST'])
+def edit_municipio(codmun):
     if 'action' in request.form:
         action = request.form['action']
         cursor = db.database.cursor()
         
         if action == 'inactivar':
-            sql = "UPDATE region SET RegEstReg='I' WHERE RegCod=%s"
-            cursor.execute(sql, (codreg,))
+            sql = "UPDATE municipio SET MunEstReg='I' WHERE MunCod=%s"
+            cursor.execute(sql, (codmun,))
         elif action == 'activar':
-            sql = "UPDATE region SET RegEstReg='A' WHERE RegCod=%s"
-            cursor.execute(sql, (codreg,))
+            sql = "UPDATE municipio SET MunEstReg='A' WHERE MunCod=%s"
+            cursor.execute(sql, (codmun,))
         elif action == 'edit':
-            nomreg = request.form['nomreg']
-            if nomreg:
+            nommun = request.form['nommun']
+            if nommun:
                 # Verificar si el nombre ya existe para otros registros
-                cursor.execute("SELECT * FROM region WHERE RegNom = %s AND RegCod != %s", (nomreg, codreg))
+                cursor.execute("SELECT * FROM municipio WHERE MunNom = %s AND MunCod != %s", (nommun, codmun))
                 existing_nom = cursor.fetchone()
                 if existing_nom:
-                    flash('El nombre de la región ya existe. Por favor ingrese un nombre diferente.')
+                    flash('El nombre del municipio ya existe. Por favor ingrese un nombre diferente.')
                     cursor.close()
                     return redirect(url_for('home'))
 
-                sql = "UPDATE region SET RegNom=%s WHERE RegCod=%s"
-                cursor.execute(sql, (nomreg, codreg))
+                sql = "UPDATE municipio SET MunNom=%s WHERE MunCod=%s"
+                cursor.execute(sql, (nommun, codmun))
         
         db.database.commit()
         cursor.close()
